1db1b1bbfc385ba2ac6a2c26ad445eeb5517d491
Scott Robinson <scott@quadhome.com>
2010-08-26 14:45:34 +1000

    Actually get some tests in there.

--- features/step_definitions/session_steps.rb
+++ features/step_definitions/session_steps.rb
@@ -1,23 +1,29 @@
+XPATH_CURRENT = "//div[contains(@class, 'current')]"
+XPATH_SCHEDULE = "#{XPATH_CURRENT}//ul[contains(@class, 'schedule')]"
 
-Given /I open the app/ do
-  $browser.goto "#{Dir.getwd}/index.html"
+Given /I open (\w+)/ do |schedule_day|
+  @browser = Watir::Safari.start "#{Dir.getwd}/index.html"
+  @browser.link(:xpath, "#{XPATH_CURRENT}//div[@class='toolbar']//a[@href='##{schedule_day}']").click
 end
 
 When /the home page appears/ do
-  $browser.title.should == "Agile 2010"
+  @browser.title.should == "Agile 2010"
 end
 
 Then /I should see (\w+) selected/ do |schedule_day|
+  @browser.link(:class, "selected").text.should == schedule_day
 end
 
-Then /the the following time slot should be (\w+)/ do |time_slot|
+Then /time slot (\d\d:\d\d(?:AM|PM)) exists/ do |time_slot|
+  @ts_query = "#{XPATH_SCHEDULE}//li[text()='#{time_slot}']"
+
+  @browser.li(:xpath, @ts_query).text.should == time_slot
 end
 
-Then /the title should be (\w+)/ do |title|
+Then /the title should be (.+)/ do |title|
+  @browser.link(:xpath, "#{@ts_query}/following-sibling::li//a[contains(@class, 'topic-link')]").text.should == title
 end
 
-Then /the speaker should be (\w+)/ do |speaker|
-end
-
-Then /the following (.+) contains the (\w+)/ do |time_slot, sessions|
+Then /the speaker should be (.+)/ do |speaker|
+  @browser.div(:xpath, "#{@ts_query}/following-sibling::li//div[contains(@class, 'speaker-go')]").text.should == speaker
 end
--- features/support/env.rb
+++ features/support/env.rb
@@ -1,10 +1,2 @@
 require 'spec/expectations'
-require "watir"
-
-Watir::Browser.default = 'safari'
-
-$browser = Watir::Browser.new
-
-Kernel::at_exit do 
-  $browser.close
-end
+require 'safariwatir'
