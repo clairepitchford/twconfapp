5275d411015c432ebb2ce2efc9aec30b27ac451d
Scott Robinson <scott@quadhome.com>
2010-08-27 11:28:27 +1000

    Added unit tests and validation for the HTML build process.

--- build-html.rb
+++ build-html.rb
@@ -1,56 +1,47 @@
 #!/usr/bin/env ruby
 
 require 'yaml'
-require 'erb'
-require 'time'
-require 'fileutils'
-require 'rubygems'
 require 'json'
 
-class Main
-  def check_speakers
-    puts "Loading topics"
-    topics = YAML::load(File.open('data/topics.yml'))
+Topic = Struct.new ('Topic', :id, :day, :time, :title, :speakers, :description)
+Speaker = Struct.new ('Speaker', :id, :name, :title, :description)
 
-    puts "Loading speakers"
-    speakers = YAML::load(File.open('data/speakers.yml'))
-    valid = true
-    topics.each_key do |id|
-      topic_speakers = topics[id]['speakers']
-      if topic_speakers!=nil
-        topic_speakers.split(',').each do |speaker_id|
-          speaker_id.strip!
-          if speakers[speaker_id] == nil
-            puts "topic #{id} points to speaker #{speaker_id} which is not in speakers.yml"
-            valid = false
-          end
-          raise "Speaker #{speaker_id} does not have a description" unless speakers[speaker_id]['description']
-          raise "Speaker #{speaker_id} does not have a title" unless speakers[speaker_id]['title']
-          raise "Speaker #{speaker_id} does not have a name" unless speakers[speaker_id]['name']
-        end
+class JSONConverter
+  attr_reader :topics, :speakers
+
+  def initialize(topics_yaml, speakers_yaml)
+    @topics = YAML::load(topics_yaml) || {}
+    @speakers = YAML::load(speakers_yaml) || {}
+
+    @topics.each_pair do |t_id, data|
+      t = DateTime.strptime(data['date'], '%a %I:%M%p')    # Will throw on parse error
+      raise "Topic #{t_id} is not on Wednesday or Thursday." unless [3, 4].include? t.wday
+      raise "Topic #{t_id} does not have a description" unless data['description']
+
+      speakers = (data['speakers'] || '').split(',').map { |s| s.strip }.each do |s_id|
+        raise "Topic #{t_id} points to a non-existant speaker #{s_id}" unless @speakers[s_id]
+        raise "Speaker #{s_id} does not have a description" unless @speakers[s_id]['description']
+        raise "Speaker #{s_id} does not have a title" unless @speakers[s_id]['title']
+        raise "Speaker #{s_id} does not have a name" unless @speakers[s_id]['name']
       end
     end
-    
-    now = DateTime.now
-    
-    data = <<-DATA
-    var defaultSpeakerData = { 
-      'timestamp': '#{now}',
-      'data': #{speakers.to_json}
-    }
-    var defaultSessionData = {
-      'timestamp': '#{now}',
-      'data': #{topics.to_json}
-    }
-    DATA
-    
-    File.open("themes/agile2010/defaultData.js", "w") { |f| f.puts data }
-    
-    return valid
+  end
+
+  def write(out)
+    out.write <<-EOF
+      var defaultSpeakerData = { 
+        'data': #{@speakers.to_json}
+      }
+      var defaultSessionData = {
+        'data': #{@topics.to_json}
+      }
+    EOF
   end
 end
 
 if __FILE__ == $0
-  main = Main.new
-  main.check_speakers
+  File.open("themes/agile2010/defaultData.js", "w") do |f|
+    JSONConverter(File.open('data/topics.yml'),
+                  File.open('data/speakers.yml')).write f
+  end
 end
