0ea634b8d7b216a20563f07918d21dd61e84b9c2
Sam Tardif <stardif@sam-mbp.local>
2010-09-03 09:58:46 +1000

    Added more tests for go-skip widgets

--- features/attend_session.feature
+++ features/attend_session.feature
@@ -8,5 +8,16 @@ Feature: As an Attendee, I want to keep track of sessions that I'm interested in
 		Then the "breakfast" session reminder should be toggled on
 		
 	Scenario: I've changed my mind about attending the second session
+		Given I open the Schedule tab
+		And open the Wednesday schedule
+		And the "breakfast" session reminder is toggled on
+		When I toggle the "breakfast" session reminder
+		Then the "breakfast" session reminder should be toggled off
 	
 	Scenario: I've quit the app and want to see the reminders I saved in a previous session
+		Given I open the Schedule tab
+		And open the Wednesday schedule
+		And the "breakfast" session reminder is toggled on
+		When I open the Schedule tab
+		And open the Wednesday schedule
+		Then the "breakfast" session reminder should be toggled on
--- features/step_definitions/attend_session_steps.rb
+++ features/step_definitions/attend_session_steps.rb
@@ -1,5 +1,6 @@
-And /the "(\w+)" session reminder is toggled off/ do |session_id|
-  if @phone.session_checked? session_id
+And /the "(\w+)" session reminder is toggled (off|on)/ do |session_id, reminder_state|
+  reminder_state = reminder_state == "on" ? true : false
+  if @phone.session_checked?(session_id) != reminder_state
     @phone.toggle_session_reminder session_id
   end
 end
@@ -8,6 +9,7 @@ When /I toggle the "(\w+)" session reminder/ do |session_id|
   @phone.toggle_session_reminder session_id
 end
 
-Then /the "(\w+)" session reminder should be toggled on/ do |session_id|
-  @phone.session_checked?(session_id).should == true
+Then /the "(\w+)" session reminder should be toggled (off|on)/ do |session_id, reminder_state|
+  reminder_state = reminder_state == "on" ? true : false
+  @phone.session_checked?(session_id).should == reminder_state
 end
