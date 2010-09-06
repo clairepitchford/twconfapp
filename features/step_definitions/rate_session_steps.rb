efbd4d03556e3d953681c218ba31abf6a3c1acc3
Sam Tardif <stardif@sam-mbp.local>
2010-09-06 10:15:25 +1000

    Fixed bug where stars were showing up on home screen before session had happened. Fixed tests to clear rating data.

--- features/step_definitions/rate_session_steps.rb
+++ features/step_definitions/rate_session_steps.rb
@@ -3,6 +3,11 @@ Given /the current time is set to "(.*?)"/ do |time_string|
   phone.browser.eval_js('localStorage.setItem("_watir_tests_fake_time_string", "'+time_string+'");')
 end
 
+Given /I have not previously rated the "(.*?)" session/ do |session_id|
+  phone = SimPhone.new
+  phone.browser.eval_js('localStorage.removeItem("breakfast-rating")');
+end
+
 When /I click the "Rate this session" icon for the "(\w+)" session/ do |session_id|
   @phone.click_rate_session session_id
 end
