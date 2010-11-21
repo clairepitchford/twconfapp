5da38e9b81a3343115998565ad832c33bc0e821b
Liauw Fendy <lfendy@lfendymbpfbagz.corporate.thoughtworks.com>
2010-11-22 10:16:21 +1100

    forgot to commit the new spec.js file

---
+++ spec/ConferenceDOMBuilderSpec.js
@@ -0,0 +1,33 @@
+describe('ConferenceDOMBuilder', function () {
+	var sessionID = '#sessionTest'
+  var session = new ConferenceSession(
+        'day',
+        {'title':'test', 'date':'Wed 07:00AM', 'speakers': null});
+
+	  it('should return an empty list when no speakers exist for this session', function () {
+		var speakerList = new ConferenceDOMBuilder(new AgileConference(null, null)).buildSessionSpeakerList(session);
+		//console.log(speakerList);
+	    // expect(speakerList.toString()).toEqual('<ul class="speaker" speakers=""></ul>');
+		expect(speakerList[0].className).toEqual("speaker");
+		expect(speakerList[0].childElementCount).toEqual(0);
+	  });	
+	
+	  it('should not return undefined when there is no session description', function () {
+		var sessionDOM = new ConferenceDOMBuilder(new AgileConference(null, null)).buildSessionDOM(sessionID,session);
+		console.log(sessionDOM.html())
+		console.log(sessionDOM);
+		console.log(sessionDOM.find('div.description'))
+		console.log(sessionDOM.find('div.description').html())
+		
+		
+		
+	    expect(getSummaryOfSession(sessionDOM)).toEqual('');
+		//expect(speakerList[0].className).toEqual("speaker");
+		//expect(speakerList[0].childElementCount).toEqual(0);
+		//expect(sessionDOM[0])
+	  });
+	
+	function getSummaryOfSession(sessionDOM){
+		return sessionDOM.find('div.description').get(1).innerText;
+	}
+});
